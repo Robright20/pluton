1b98f160fa3cc64a3e102716254f4edcc73ff7e5
abderrahman alhaouil <aalhaoui@student.1337.ma>
2021-03-08 19:22:34 +0100

    fix some error

--- src/verify_champ.c
+++ src/verify_champ.c
@@ -6,7 +6,7 @@
 /*   By: aalhaoui <aalhaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 15:26:45 by mac               #+#    #+#             */
-/*   Updated: 2021/02/28 17:46:25 by aalhaoui         ###   ########.fr       */
+/*   Updated: 2021/03/08 19:19:17 by aalhaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,33 @@
 int		check_champ_file(t_players *players, char *champ, int id)
 {
 	unsigned int	magic_header;
+	char			*tmp;
 	int				fd;
 	int				ret;
 
+	tmp = NULL;
 	if ((fd = open(champ, O_RDONLY)) < 0)
 	{
 		perror("champ");
 		return (-1);
 	}
 	if ((magic_header = convert_to_numfd(4, fd)) != COREWAR_EXEC_MAGIC)
-		return (-1);
+		return (-2);
 	if (read(fd, players->player[id]->name, PROG_NAME_LENGTH) != 128)
 		return (-1);
 	if (convert_to_numfd(4, fd) != 0)
-		return (-1);
+		return (-4);
 	if ((players->player[id]->size = convert_to_numfd(4, fd)) < 0 ||
 				players->player[id]->size > 682)
-		return (-1);
+		return (-3);
 	if (read(fd, players->player[id]->comment, COMMENT_LENGTH) != 2048)
 		return (-1);
 	if (convert_to_numfd(4, fd) != 0)
 		return (-1);
-	if ((ret = read(fd, players->player[id]->code, players->player[id]->size))
-				> 682 || ret < 0)
-		return (-1);
+	if ((ret = read(fd, players->player[id]->code, players->player[id]->size)))
+		if ((ret != players->player[id]->size) || read(fd, tmp, 1))
+			return (-4);
+	printf("%d %d\n", ret, players->player[id]->size);
 	close(fd);
 	return (1);
 }
@@ -47,18 +50,26 @@ int		verify_champ(t_players *players, char *argv, int id)
 {
 	int		champ_len;
 	char	*extension;
+	int		ret;
 
 	champ_len = ft_strlen(argv);
 	if (!(extension = ft_strsub(argv, champ_len - 4, champ_len)))
 		return (-1);
 	if (!ft_strequ(extension, ".cor"))
 	{
-		printf("ERROR : champion extension error");
+		write(2, "ERROR : champion file not valid\n", 31);
 		return (-1);
 	}
-	if (check_champ_file(players, argv, id) < 0)
+	if ((ret = check_champ_file(players, argv, id)) < 0)
 	{
-		printf("ERROR : champion file not valid");
+		if (ret == -3)
+			write(2, "Error - Code is too large (max 682 bytes)\n", 42);
+		else if (ret == -2)
+			write(2, "Error - Invalid file signature", 30);
+		else if (ret == -4)
+			write(2, "Error - Inconsistent number of instructions\n", 44);
+		else
+			write(2, "ERROR : champion file not valid\n", 31);
 		return (-1);
 	}
 	return (1);
