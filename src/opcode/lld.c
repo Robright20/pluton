5e286b301955d57f30cec130f529a3bc315c03ee
abderrahman alhaouil <aalhaoui@student.1337.ma>
2021-03-10 19:14:22 +0100

    working to finish tests

--- src/opcode/lld.c
+++ src/opcode/lld.c
@@ -6,7 +6,7 @@
 /*   By: aalhaoui <aalhaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 16:20:27 by aalhaoui          #+#    #+#             */
-/*   Updated: 2021/03/10 17:21:19 by aalhaoui         ###   ########.fr       */
+/*   Updated: 2021/03/10 19:13:47 by aalhaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ int		lld(t_cursor *processes, t_game_para *parameters, int *size)
 		if (size[0] == 3)
 		{
 			index = (processes->pc + processes->args[0]) % MEM_SIZE;
-			processes->args[0] = get_vfarena(parameters->arena, 4, index);
+			processes->args[0] = get_vfarena(parameters->arena, 2, index);
 		}
 		if ((parameters->verbos >> 2) & 1)
-			printf("P %4d | %s %d r%d\n", processes->id,
-			op_tab[processes->opcode - 1].name, processes->args[0],
+			printf("P %4d | lld %d r%d\n", processes->id, processes->args[0],
 			processes->args[1]);
 		processes->registeries[processes->args[1] - 1] = processes->args[0];
 		(processes->args[0] == 0) && (processes->carry = 1);
