1a900e5d4047a091c0876aca77cda4efbbcad6d7
Robert Bright <robright28@gmail.com>
2021-03-14 05:34:03 +0100

    add visualizer

---
+++ src/dataLib.js
@@ -0,0 +1,21 @@
+// function Cell() {
+// 	this.idx = 0;
+// 	this.text = {
+// 		value: "",
+// 		fontFamily: "",
+// 		color: ""
+// 		size: 0,
+// 	}
+// 	this.bgColor = "[color]";
+// 	this.border = "[color]";
+// }
+// //(4*1024)
+// //(MEM_SIZE / 6)
+// function drawCell(cell) {
+// 	if (!cell instanceof Cell) {
+// 		return false;
+// 	}
+// 	ctx.strokeRect(0, 0, 10, 10);
+// 	ctx.font = '8px serif';
+// 	ctx.fillText('2f', 0, 0, 10);
+// }
