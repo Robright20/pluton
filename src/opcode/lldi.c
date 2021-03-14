5db53ba3823fafe316e71e597c4ab49b06c9a50e
abderrahman alhaouil <aalhaoui@student.1337.ma>
2021-03-14 17:32:46 +0100

    fix error come from return

--- src/opcode/lldi.c
+++ src/opcode/lldi.c
@@ -6,7 +6,7 @@
 /*   By: aalhaoui <aalhaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 15:12:11 by aalhaoui          #+#    #+#             */
-/*   Updated: 2021/03/12 16:23:40 by aalhaoui         ###   ########.fr       */
+/*   Updated: 2021/03/14 17:32:05 by aalhaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int			lldi(t_cursor *processes, t_game_para *parameters, int *size)
 
 	if (processes->args[2] > 0 && processes->args[2] < 17)
 	{
-		if_size_3(processes, parameters, size);
+		processes = if_size_3(processes, parameters, size);
 		if ((parameters->verbos >> 2) & 1)
 			printf("P %4d | lldi %d %d r%d\n", processes->id,
 				processes->args[0], processes->args[1], processes->args[2]);
