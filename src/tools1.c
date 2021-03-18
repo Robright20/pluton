18405e07addc87ff6283d26f04dfa8327cac79e1
Robert Bright <robright28gmail.com>
2021-03-18 19:41:36 +0100

    in your corewar

--- src/tools1.c
+++ src/tools1.c
@@ -14,18 +14,18 @@
 
 int			print_usage(void)
 {
-	ft_printf("Usage: ./corewar [-d N -v N | -n N <champion1.cor> <...>\n");
-	ft_printf(" -a / --aff         : Prints output from \"aff\" (Default is to\
- hide it)\n");
-	ft_printf(" -d / --dump   N    : Dumps memory after N cycles then exits\n");
-	ft_printf(" -v / --verbos N    : Verbosity levels, can be added together ");
-	ft_printf("to enable several\n");
-	ft_printf("			- 0 : Show only essentials (introduction + ");
-	ft_printf("the winner)\n");
-	ft_printf("			- 1 : Show lives\n");
-	ft_printf("			- 2 : Show cycles\n");
-	ft_printf("			- 4 : Show operations (Params are NOT litteral ...)\n");
-	ft_printf("			- 8 : Show deaths\n");
+	ft_printf("Usage: ./corewar [-d N -v N | -n N <champion1.cor> <...>\n"
+		" -a / --aff         : Prints output from \"aff\" (Default is to"
+		"hide it)\n"
+		" -d / --dump   N    : Dumps memory after N cycles then exits\n"
+		" -v / --verbos N    : Verbosity levels, can be added together "
+		"to enable several\n"
+		"			- 0 : Show only essentials (introduction + "
+		"the winner)\n"
+		"			- 1 : Show lives\n"
+		"			- 2 : Show cycles\n"
+		"			- 4 : Show operations (Params are NOT litteral ...)\n"
+		"			- 8 : Show deaths\n");
 	return (1);
 }
 
